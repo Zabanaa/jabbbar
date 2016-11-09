c17169f5ba025f27b3979b41a42776feef25411d
= <=>
2016-11-09 16:10:26 +0000

    test generate_auth_url

--- zabbbana/tests/test.py
+++ zabbbana/tests/test.py
@@ -4,12 +4,22 @@ from zabbbana import Zabbbana
 
 class BaseTest(unittest.TestCase):
 
+    maxDiff = None
+
     def setUp(self):
         self.api = Zabbbana(
             client_id=CLIENT_ID,
             client_secret=CLIENT_SECRET,
+            scope=['write'],
+            state="myrandomkey",
             redirect_uri=REDIRECT_URI
         )
+
+    def test_generate_auth_url(self):
+        expected_url = "https://dribbble.com/oauth/authorize?client_id={}&redirect_uri={}&scope={}&state={}"\
+                        .format(self.api.client_id, self.api.redirect_uri, self.api.scope, self.api.state)
+        auth_url     = self.api.generate_auth_url
+        self.assertEqual(auth_url, expected_url)
 
 if __name__ == "__main__":
     unittest.main()
