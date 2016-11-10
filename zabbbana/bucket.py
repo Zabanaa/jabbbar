e6f446b21e0040d169765f230fff4df550db6fc8
= <=>
2016-11-10 14:07:47 +0000

    create Bucket.create() method

--- zabbbana/bucket.py
+++ zabbbana/bucket.py
@@ -8,6 +8,12 @@ class Bucket(zbn):
         self.bucket_id = bucket_id
 
     def get_details(self):
-        bucket_request = req.get("{}/buckets/{}".format(zbn.API_ENDPOINT, self.bucket_id), headers=self.headers)
+        bucket_request = req.get("{}/buckets/{}".format(zbn.API_ENDPOINT, self.bucket_id), headers=self.auth_header)
         details = bucket_request.json()
         return details
+
+    def create(self, name=None, description=None):
+        bucket_data     = {'name': name, 'description': description}
+        post_bucket     = req.post("{}/buckets".format(zbn.API_ENDPOINT), headers=self.auth_header, data=bucket_data)
+        response        = post_bucket.json()
+        return response
