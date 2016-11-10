345f02307ff511245c363605cb98a4b9dc8c0a6e
= <=>
2016-11-10 10:50:57 +0000

    extract self.api outside the main class

--- zabbbana/tests/test.py
+++ zabbbana/tests/test.py
@@ -2,25 +2,22 @@ import unittest
 from .credentials import *
 from zabbbana import Zabbbana
 
+jordan = Zabbbana(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, access_token=ACCESS_TOKEN, state="myrandomkey", redirect_uri=REDIRECT_URI)
+
 class BaseTest(unittest.TestCase):
 
     maxDiff             = None
     default_bucket_id   = "2754"
 
     def setUp(self):
-        self.api = Zabbbana(
-            client_id=CLIENT_ID,
-            client_secret=CLIENT_SECRET,
-            state="myrandomkey",
-            redirect_uri=REDIRECT_URI
-        )
-        self.api.access_token = ACCESS_TOKEN
+        self.api = jordan
 
     def test_generate_auth_url(self):
         expected_url = "https://dribbble.com/oauth/authorize?client_id={}&redirect_uri={}&scope={}&state={}"\
                         .format(self.api.client_id, self.api.redirect_uri, self.api.scope, self.api.state)
         auth_url     = self.api.generate_auth_url
         self.assertEqual(auth_url, expected_url)
+
 
 if __name__ == "__main__":
     unittest.main()
