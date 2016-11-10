1dba1942872e0fb19fa09382df695fc207b61297
= <=>
2016-11-10 11:18:28 +0000

    inherit from Zabbbana using kw args

--- zabbbana/bucket.py
+++ zabbbana/bucket.py
@@ -3,8 +3,8 @@ import requests as req
 
 class Bucket(zbn):
 
-    def __init__(self, client, bucket_id):
-        zbn.__init__(self, client.client_id, client.client_secret, client.access_token)
+    def __init__(self, inst, bucket_id):
+        zbn.__init__(self, client_id=inst.client_id, client_secret=inst.client_secret, access_token=inst.access_token)
         self.bucket_id = bucket_id
 
     def get_details(self):
