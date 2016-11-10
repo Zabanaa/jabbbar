cd275e53ddf3ef4d690816f49268f470eda2dcf4
= <=>
2016-11-10 15:35:01 +0000

    create Bucket.delete() test

--- zabbbana/tests/bucket.py
+++ zabbbana/tests/bucket.py
@@ -13,19 +13,23 @@ class BucketTest(unittest.TestCase):
         self.bucket = Bucket(jordan, 2754)
 
     def test_get_details(self):
-        details = self.bucket.get_details()
-        self.assertIn('shots_count', details)
+        response = self.bucket.get_details()
+        self.assertIn('shots_count', response)
 
     def test_create_bucket(self):
         bucket_name         = "my new name"
         bucket_description  = "new cool bucket"
-        post_bucket         = self.bucket.create(name=bucket_name, description=bucket_description)
-        self.assertIn('created_at', post_bucket)
+        response            = self.bucket.create(name=bucket_name, description=bucket_description)
+        self.assertIn('created_at', response)
 
     def test_update_bucket(self):
         bucket_new_name = "bruv wagwan"
-        response        = self.bucket.update(bucket_id=442460, name=bucket_new_name)
+        response        = self.bucket.update(bucket_id=442496, name=bucket_new_name)
         self.assertEqual(bucket_new_name, response['name'])
+
+    def delete_bucket(self):
+        response    = self.bucket.delete(bucket_id=442503)
+        self.assertEqual(204, response.__dict__['status_code'])
 
 if __name__ == "__main__":
     unittest.main()
