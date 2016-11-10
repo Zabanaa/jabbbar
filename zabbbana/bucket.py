83e47870cc4ec4b4f841162b2d35d11631bf1302
= <=>
2016-11-10 13:48:28 +0000

    create Bucket.get_details() method

--- zabbbana/bucket.py
+++ zabbbana/bucket.py
@@ -9,4 +9,6 @@ class Bucket(zbn):
 
     def get_details(self):
         headers = {'Authorization': 'Bearer {}'.format(self.access_token)}
-        bucket_details = req.get("{}/buckets/{}", headers=headers).format(zbn.API_ENDPOINT, self.bucket_id)
+        bucket_request = req.get("{}/buckets/{}".format(zbn.API_ENDPOINT, self.bucket_id), headers=headers)
+        details = bucket_request.json()
+        return details
