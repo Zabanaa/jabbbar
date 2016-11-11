fc05285c3736e4482c24e7c9b0a4dd4c0193671e
= <=>
2016-11-11 17:55:12 +0000

    create Project.get_shots() method and check for project_id in both methods

--- zabbbana/project.py
+++ zabbbana/project.py
@@ -4,10 +4,28 @@ import requests as req
 class Project(zbn):
     MAIN_ENDPOINT = "{}/projects".format(zbn.API_ENDPOINT)
 
-    def __init__(self, inst):
+    def __init__(self, inst, project_id):
         zbn.__init__(self, client_id=inst.client_id, client_secret=inst.client_secret, access_token=inst.access_token)
+        self.project_id = project_id
 
     def get_details(self, project_id=None):
+
+        if project_id:
+            project_id = project_id
+        else:
+            project_id = self.project_id
+
         project_details = req.get("{}/{}".format(self.MAIN_ENDPOINT, project_id), headers=self.auth_header)
         response        = project_details.json()
+        return response
+
+    def get_shots(self, project_id=None):
+
+        if project_id:
+            project_id = project_id
+        else:
+            project_id = self.project_id
+
+        shots    = req.get("{}/{}/shots".format(self.MAIN_ENDPOINT, project_id), headers=self.auth_header)
+        response = shots.json()
         return response
