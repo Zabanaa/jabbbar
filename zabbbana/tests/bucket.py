eac933b089f0ee9644c68c006cc12708321753ff
= <=>
2016-11-11 15:02:09 +0000

    clean up the tests

--- zabbbana/tests/bucket.py
+++ zabbbana/tests/bucket.py
@@ -3,9 +3,12 @@ from .credentials import *
 from zabbbana import Zabbbana
 from zabbbana.bucket import Bucket
 
-jordan = Zabbbana(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, redirect_uri=REDIRECT_URI,\
-                  access_token=ACCESS_TOKEN, state="hello")
-my_bucket = Bucket(jordan, 2754)
+EXTERNAL_BUCKET_ID = 2755
+BUCKET_TO_UPDATE   = 442480
+DEFAULT_SHOT_ID     = 471756
+
+jordan = Zabbbana(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, access_token=ACCESS_TOKEN, state="myrandomkey",\
+                  redirect_uri=REDIRECT_URI)
 
 class BucketTest(unittest.TestCase):
 
@@ -21,10 +24,16 @@ class BucketTest(unittest.TestCase):
         bucket_description  = "new cool bucket"
         response            = self.bucket.create(name=bucket_name, description=bucket_description)
         self.assertIn('created_at', response)
+        self.bucket.delete(bucket_id=response['id']) # Immediately delete the created bucket to avoid bloat
 
     def test_update_bucket(self):
         bucket_new_name = "bruv wagwan"
-        response        = self.bucket.update(bucket_id=442496, name=bucket_new_name)
+        response        = self.bucket.update(bucket_id=BUCKET_TO_UPDATE, name=bucket_new_name)
+        self.assertEqual(bucket_new_name, response['name'])
+
+    def test_update_without_id(self):
+        bucket_new_name = "Jobs board for python developers"
+        response        = self.bucket.update(name=bucket_new_name)
         self.assertEqual(bucket_new_name, response['name'])
 
     def delete_bucket(self):
@@ -36,15 +45,15 @@ class BucketTest(unittest.TestCase):
         self.assertIs(type(response), list)
 
     def test_list_all_shots_with_external_id(self):
-        response    = self.bucket.list_shots(bucket_id=2755)
+        response    = self.bucket.list_shots(bucket_id=EXTERNAL_BUCKET_ID)
         self.assertIs(type(response), list)
 
     def test_add_shot(self):
-        response    = self.bucket.add_shot(shot_id=471756)
+        response    = self.bucket.add_shot(shot_id=DEFAULT_SHOT_ID)
         self.assertEqual(204, response.__dict__['status_code'])
 
     def test_remove_shot(self):
-        response    = self.bucket.remove_shot(shot_id=471756)
+        response    = self.bucket.remove_shot(shot_id=DEFAULT_SHOT_ID)
         self.assertEqual(204, response.__dict__['status_code'])
 
 if __name__ == "__main__":
