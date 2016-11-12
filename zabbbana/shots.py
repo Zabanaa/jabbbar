7b5e865d7359e2c3d7c6476e31f45d9990f89905
= <=>
2016-11-12 15:35:06 +0000

    create Shots class and Shots.list_all() method

---
+++ zabbbana/shots.py
@@ -0,0 +1,18 @@
+from zabbbana import Zabbbana as zbn
+import requests as req
+
+class Shots(zbn):
+
+    MAIN_ENDPOINT = "{}/shots".format(zbn.API_ENDPOINT)
+
+    def __init__(self, inst):
+        zbn.__init__(
+            self,
+            client_id=inst.client_id,
+            client_secret=inst.client_secret,
+            access_token=inst.access_token
+        )
+
+    def list_all(self, params):
+        shots = req.get(self.MAIN_ENDPOINT, headers=self.auth_header, params=params)
+        return shots.json()
