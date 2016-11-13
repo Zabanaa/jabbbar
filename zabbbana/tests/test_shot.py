2ce40c8239a96da97857491927fdb66eb526d775
= <=>
2016-11-13 12:07:17 +0000

    prepare test functions for Shot class

--- zabbbana/tests/test_shot.py
+++ zabbbana/tests/test_shot.py
@@ -4,3 +4,47 @@ from . import jordan
 DEFAULT_SHOT_ID = 334543
 test_shot = Shot(jordan, DEFAULT_SHOT_ID)
 
+def test_list_attachements():
+    pass
+
+def test_get_attachement():
+    pass
+
+def test_list_buckets():
+    pass
+
+def test_list_comments():
+    pass
+
+def test_list_likes():
+    pass
+
+def test_get_comment():
+    pass
+
+def test_check_user_likes_comment():
+    pass
+
+def test_like_comment():
+    pass
+
+def test_unlike_comment():
+    pass
+
+def test_list_likes():
+    pass
+
+def test_check_user_likes_shot():
+    pass
+
+def test_like_shot():
+    pass
+
+def test_unlike_shot():
+    pass
+
+def test_list_projects():
+    pass
+
+def test_list_rebounds():
+    pass
