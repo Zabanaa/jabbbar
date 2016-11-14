5d4824308bcc16671a86de3df516ba9cdb7d0a76
= <=>
2016-11-14 16:36:42 +0000

    rename generate_auth_url to auth_url

--- zabbbana/tests/__init__.py
+++ zabbbana/tests/__init__.py
@@ -7,5 +7,5 @@ jordan = Zabbbana(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, access_token
 def test_generate_auth_url():
     expected_url = "https://dribbble.com/oauth/authorize?client_id={}&redirect_uri={}&scope={}&state={}"\
                     .format(jordan.client_id, jordan.redirect_uri, jordan.scope, jordan.state)
-    auth_url     = jordan.generate_auth_url
+    auth_url     = jordan.auth_url
     assert auth_url == expected_url
