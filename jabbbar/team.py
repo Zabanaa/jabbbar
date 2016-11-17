bdc230af4499771102f6ac94f735c00483c17994
= <=>
2016-11-17 11:03:47 +0000

    fix alignment in the methods

--- jabbbar/team.py
+++ jabbbar/team.py
@@ -1,8 +1,8 @@
 class Team():
 
     def __init__(self, client, team_name):
-        self.client  = client
-        self.team_name = str(team_name)
+        self.client     = client
+        self.team_name  = str(team_name)
 
 
     def list_players(self, team_name=None):
@@ -10,8 +10,8 @@ class Team():
             List a team's players
             http://developer.dribbble.com/v1/teams/members/
         """
-        team_name             = str(team_name) if team_name is not None else self.team_name
-        team_players        = self.client.GET("/teams/{}/members".format(team_name))
+        team_name               = str(team_name) if team_name is not None else self.team_name
+        team_players            = self.client.GET("/teams/{}/members".format(team_name))
         return team_players.json()
 
 
@@ -19,6 +19,6 @@ class Team():
         """ List a team's shots
             http://developer.dribbble.com/v1/teams/shots/
         """
-        team_name             = str(team_name) if team_name is not None else self.team_name
-        team_shots          = self.client.GET("/teams/{}/shots".format(team_name))
+        team_name               = str(team_name) if team_name is not None else self.team_name
+        team_shots              = self.client.GET("/teams/{}/shots".format(team_name))
         return team_shots.json()
