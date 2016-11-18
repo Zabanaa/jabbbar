248793b3299afb067f49c7cac0204d2b72dc971e
= <=>
2016-11-18 14:14:00 +0000

    refactor auth_url test

--- jabbbar/tests/__init__.py
+++ jabbbar/tests/__init__.py
@@ -17,7 +17,6 @@ client = Jabbbar(client_id=CLIENT_ID, client_secret=CLIENT_SECRET, access_token=
                   redirect_uri=REDIRECT_URI)
 
 def test_generate_auth_url():
-    expected_url = "https://dribbble.com/oauth/authorize?client_id={}&redirect_uri={}&scope={}&state={}"\
-                    .format(client.client_id, client.redirect_uri, client.scope, client.state)
     auth_url     = client.auth_url
-    assert auth_url == expected_url
+    assert CLIENT_ID in auth_url
+    assert REDIRECT_URI in auth_url
